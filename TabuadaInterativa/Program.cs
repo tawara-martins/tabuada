c0a976af3603fefcbf40e3a893b8978470844e26
Tawara Martins <tawaramartinsaraujo@gmail.com>
2025-08-09 10:28:00 -0300

    Projeto inicializado e finalizado

---
+++ TabuadaInterativa/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Program
+{
+    class TabuadaInterativa
+    {
+       public void Main(string[] args)
+        {
+            // Limpa o console para uma melhor visualização
+            Console.Clear();
+
+            // Invoca o método
+            GerarTabuada();
+        }
+
+        static void GerarTabuada()
+        {
+            // Solicita ao usuário o número para a tabuada
+            Console.WriteLine($"Vamos gerar uma tabuada para você!");
+            Console.WriteLine($"Digite um número: ");
+
+            // Lê o número digitado pelo usuário
+            int numero = int.Parse(Console.ReadLine());
+
+            // Exibe a tabuada do número informado
+            Console.WriteLine($"Tabuada do número {numero}");
+
+            // Gere a tabuada do número informado pelo usuário e o resultado
+            for (int i = 0; i <= 10; i++)
+            {
+                int resultado = numero * i;
+                Console.WriteLine($"{numero} x {i} = {resultado}");
+            }
+
+            return;
+        }
+    }
+}
